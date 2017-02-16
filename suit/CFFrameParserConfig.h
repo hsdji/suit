4d6a0e4976e6e1e0ae73e2e1f9785d4289c9f993
@单小飞 <shanpengfei@zhidaochuxing.com>
2017-02-16 15:23:19 +0800

    CTFrameOasrserConfig类 ，用于配置绘制的参数，例如文字的颜色、大小、行间距等等.............

---
+++ suit/CFFrameParserConfig.h
@@ -0,0 +1,13 @@
+//
+//  CFFrameParserConfig.h
+//  suit
+//
+//  Created by ekhome on 17/2/16.
+//  Copyright © 2017年 xiaofei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CFFrameParserConfig : NSObject
+
+@end
---
+++ suit/CFFrameParserConfig.m
@@ -0,0 +1,13 @@
+//
+//  CFFrameParserConfig.m
+//  suit
+//
+//  Created by ekhome on 17/2/16.
+//  Copyright © 2017年 xiaofei. All rights reserved.
+//
+
+#import "CFFrameParserConfig.h"
+
+@implementation CFFrameParserConfig
+
+@end
